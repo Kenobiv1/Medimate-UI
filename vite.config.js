7842f6cc30e71041de6aec9cc6b4dc32042fdfff
Kenobiv1 <kenobiv1000@gmail.com>
2025-02-25 23:08:12 +0800

    Configure Vite for deployment and add gh-pages support

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  // Set the base to your repository name
+  base: '/medimate-ui/',
   plugins: [react()],
 })
